c38a1c45a03afa309428d2ab67f6196c0d1f992e
wanpc3 <wanilhanidrisswanabdulkadir@gmail.com>
2024-09-16 12:31:33 +0800

    Update dictionary_utils.c

--- dictionary_utils.c
+++ dictionary_utils.c
@@ -1,39 +1,24 @@
 #include "portuguese.h"
+#include <wctype.h> // For wide character functions
 
 void welcome_notes_PT()
 {
-	printf("---Welcome to Portuguese language dictionary!---\n");
+    wprintf(L"---Welcome to the Portuguese language dictionary!---\n");
 }
 
-int ft_isalpha(int c)
+int ft_isalpha(wchar_t c)
 {
-	if (ft_islower(c) || ft_isupper(c))
-		return (1);
-	return (0);
+    return iswalpha(c);
 }
 
-int ft_islower(int c)
+int word_checker(const wchar_t *word)
 {
-	if ((c >= 'a' && c <= 'z') || (c >= 224 && c <= 255))
-		return (1);
-	return (0);
-}
-
-int ft_isupper(int c)
-{
-	if ((c >= 'A' && c <= 'Z') || (c >= 192 && c <= 223))
-		return (1);
-	return (0);
-}
-
-int word_checker(const char *word)
-{
-	for (int i = 0; word[i] != '\0'; i++)
-	{
-		if (!ft_isalpha((unsigned char)word[i]))
-		{
-			return (0);
-		}
-	}
-	return (1);
+    for (int i = 0; word[i] != L'\0'; i++)
+    {
+        if (!ft_isalpha(word[i]))
+        {
+            return 0; // Not a valid alphabetic character
+        }
+    }
+    return 1; // Word is valid
 }
